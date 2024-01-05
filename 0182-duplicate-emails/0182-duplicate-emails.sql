f622e96df50b3ca27589140816ea7e994e68c953
Abdullah Mustafa <129633988+AbdualahMostafa@users.noreply.github.com>
2024-01-05 09:19:58 +0200

    Time: 811 ms (73.03%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,7 +1,8 @@
 /* Write your T-SQL query statement below */
 
 
-select Email
-from Person p
-group by email
-having count(email)>1
+select p.email
+from person p,person pp
+where p.email=pp.email
+group by p.email
+having count(p.email)>1
