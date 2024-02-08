6e8516402394a266c5bc851091c84a57a3f00a60
Abdullah Mustafa <129633988+AbdualahMostafa@users.noreply.github.com>
2024-02-08 23:59:20 +0200

    Time: 4 ms (86.28%), Space: 8.6 MB (7.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size(),m=t.size();
+        if(n!=m){
+            return false;
+        }
+        vector<int>v(26,0);
+       for(int i=0; i<n; i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(v[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
